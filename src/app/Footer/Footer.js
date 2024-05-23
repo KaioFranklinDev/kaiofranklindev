5cb80abff67f5a538a2936a1b8c5ad05d960ac10
KaioFranklinDev <kaio.franklin.dev@gmail.com>
2024-05-23 11:54:26 -0300

    mundaça de link privacidade

--- src/app/Footer/Footer.js
+++ src/app/Footer/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
           <a target="_blank" rel="noopener noreferrer" href="https://github.com/KaioFranklinDev" className={styles.socialLink}><FaGithub /></a>
           <a target="_blank" rel="noopener noreferrer" href="https://api.whatsapp.com/send/?phone=558699453112&text&type=phone_number&app_absent=0" className={styles.socialLink}><IoLogoWhatsapp /></a>
         </div>
-        <div className={styles.footerLinks}><a href='#' target="_blank" rel="noopener noreferrer">Privacidade</a></div>
+        <div className={styles.footerLinks}><a href='https://pt.wikipedia.org/wiki/Privacidade' target="_blank" rel="noopener noreferrer">Privacidade</a></div>
         <p>&copy; 2024 Kaio Franklin Dev. All rights reserved.</p>
       </div>
     </footer>
